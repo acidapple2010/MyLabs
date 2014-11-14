91786d898c98808a2b8332a29cf75214b0bf37d5
acidapple2010 <acidapple2010@mail.ru>
2014-11-14 18:14:59 +0300

    lab4
    lab4

--- lab4/src/com/example/myapp4/MyActivity.java
+++ lab4/src/com/example/myapp4/MyActivity.java
@@ -22,15 +22,16 @@ public class MyActivity extends Activity {
 
     private ArrayAdapter<String> adapter;
     private ListView listView;
+
     public void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         listView = (ListView) findViewById(R.id.listView);
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+
         WeatherFetcher();
         listView.setAdapter(adapter);
-
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
@@ -45,20 +46,19 @@ public class MyActivity extends Activity {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId() == R.id.renew){
-            setList(arrayCity,arrayTemp,arrayHumidity);
+        if (item.getItemId() == R.id.renew) {
+            setList(arrayCity, arrayTemp, arrayHumidity);
             adapter.notifyDataSetChanged();
         }
         return false;
     }
 
     private ArrayList<String> list = new ArrayList<String>();
-    public String[] url = {
-            "http://api.openweathermap.org/data/2.5/find?lat=55.75222&lon=37.61556&cnt=1",
-            "http://api.openweathermap.org/data/2.5/find?lat=48.85341&lon=2.3488&cnt=1",
-            "http://api.openweathermap.org/data/2.5/find?lat=51.50853&lon=-0.12574&cnt=1" };
+    private double[] phfi = new double[]{55.75222, 48.85341, 51.50853, 38.89511, 39.9075};
+    private double[] alpf = new double[]{37.61556, 2.3488, -0.12574, -77.03637, 116.39723};
 
     private String city;
     private double temp;
@@ -66,13 +66,16 @@ public class MyActivity extends Activity {
     private ArrayList arrayCity = new ArrayList<String>();
     private ArrayList arrayTemp = new ArrayList<Double>();
     private ArrayList arrayHumidity = new ArrayList<Double>();
+    private String url=null;
 
     private void WeatherFetcher() {
-        for(String iUrl:url) {
-            AsyncDataFetcher dataFetcher1 = new AsyncDataFetcher(iUrl, new DataFetcherInterface<JSONObject>() {
+        for(int i=0; i<alpf.length; i++) {
+            url = "http://api.openweathermap.org/data/2.5/find?lat=" + phfi[i] + "&lon=" + alpf[i] + "&cnt=1";
+            AsyncDataFetcher dataFetcher1 = new AsyncDataFetcher(url, new DataFetcherInterface<JSONObject>() {
                 @Override
                 public void startLoading() {
                 }
+
                 @Override
                 public void result(JSONObject echo) {
                     Log.e("Echo", echo.toString());
@@ -89,14 +92,12 @@ public class MyActivity extends Activity {
                             arrayCity.add(city);
                             arrayTemp.add(temp);
                             arrayHumidity.add(humidity);
-
-                            //arrayCTH.add(i, ((String)city+ (Double)temp + (Double)humidity));
-
                         }
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
+
                 @Override
                 public void finishLoading() {
                 }
@@ -116,7 +117,7 @@ public class MyActivity extends Activity {
         this.arrayHumidity = arrayHumidity;
 
         for (int i = 0; i < arrayCity.size(); i++) {
-            list.add((String) arrayCity.get(i)+"  "+(Double) arrayTemp.get(i)+"  "+ (Double) arrayHumidity.get(i));
+            list.add(arrayCity.get(i)+"  t°="+arrayTemp.get(i) +"°C;  "+arrayHumidity.get(i)+"%");
         }
     }
 }
